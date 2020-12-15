6f2dee01f8aceb534ddb708b2099cf95c02f0fde
Charlie <charlyfinos@gmail.com>
2020-12-15 13:31:55 -0300

    Shuffle working on frontend

--- src/components/Noun.js
+++ src/components/Noun.js
@@ -1,14 +1,15 @@
 import React, { useEffect } from 'react'
 import { Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 
-const Noun = ({ card, shuffle }) => {
+const Noun = ({ card, shuffle, handler, revolver }) => {
 
 
     return (
         <Card
+            onClick={handler}
             style={{ width: "12rem", height: "19rem" }}
-            key={card._id}
             className='my-4 p-3 rounded'>
             <Card.Img variant='top' src={card.image} />
             <Card.Body style={{ position: 'absolute', bottom: '0' }}>
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,31 +4,35 @@ import { Row, Col } from 'react-bootstrap'
 import cards from '../cards'
 
 const HomeScreen = () => {
+    const [randCards, setRandCards] = useState([cards])
+    const [count, setCount] = useState(0)
 
-    const [newCards, setNewCards] = useState(cards)
+    const rCard = cards
 
-    const shuffle = (newRandCards) => {
-        for (let i = newRandCards.length - 1; i > 0; i--) {
+    const shuffle = (rCard) => {
+        for (let i = rCard.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
-            [newRandCards[i], newRandCards[j]] = [newRandCards[j], newRandCards[i]];
+            [rCard[i], rCard[j]] = [rCard[j], rCard[i]];
         }
+        console.log(rCard)
+        setRandCards([rCard])
     }
 
+    const handler = () => {
+        shuffle(rCard)
+    }
 
     useEffect(() => {
-        const newRandCards = [...newCards]
-        shuffle(newRandCards)
-        setNewCards(newCards)
-        console.log(newCards)
-    }, [newCards])
+
+    }, [rCard])
 
     return (
         <div>
             <Row>
-                {newCards.map((card) => {
+                {rCard.map((card) => {
                     return (
-                        <Col >
-                            <Noun shuffle={shuffle} card={card} />
+                        <Col key={card._id} >
+                            <Noun handler={handler} card={card} />
                         </Col>
                     )
                 })}
