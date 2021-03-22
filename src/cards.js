fa7afa79ff62e0c4663170567294b8759f67b731
Charlie <charlyfinos@gmail.com>
2021-03-22 18:25:05 -0300

    Fruit images from aws

--- src/cards.js
+++ src/cards.js
@@ -2,58 +2,58 @@ const cards = [
     {
         _id: '1',
         name: 'Banana',
-        image: 'https://media.istockphoto.com/photos/banana-picture-id1184345169?k=6&m=1184345169&s=612x612&w=0&h=I159iiNId_-XwGsoZlpi9WyeACv8kpg-EmyB5X2oo30=',
+        image: 'https://memory-card123.s3-sa-east-1.amazonaws.com/images/banana.jpg',
         isClicked: false
     },
     {
         _id: '2',
         name: 'Apple',
-        image: 'https://sc04.alicdn.com/kf/Uf4c62ba9db5c4371a07c52c140f7054cG.jpg',
+        image: 'https://memory-card123.s3-sa-east-1.amazonaws.com/images/apple.jpg',
         isClicked: false
     },
     {
         _id: '3',
         name: 'Pineapple',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4dvad2WVxWiL5aGxULnl57XxpyVIMpSpaMA_h1YbpldFMF4Wn0it1FMlJBmoqvJPirEY&usqp=CAU',
+        image: 'https://memory-card123.s3-sa-east-1.amazonaws.com/images/pineapple-550x550.jpg',
         isClicked: false
     },
     {
         _id: '4',
         name: 'Watermelon',
-        image: 'https://img.freepik.com/free-photo/watermelon-isolated-white-background_29402-628.jpg?size=626&ext=jpg',
+        image: 'https://memory-card123.s3-sa-east-1.amazonaws.com/images/watermelon.jpg',
         isClicked: false
     },
     {
         _id: '5',
         name: 'Cherry',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Cherry_Stella444.jpg/600px-Cherry_Stella444.jpg',
+        image: 'https://memory-card123.s3-sa-east-1.amazonaws.com/images/cherry.jpg',
         isClicked: false
     },
     {
         _id: '6',
         name: 'Mango',
-        image: 'http://www.gm-importexport.com/uploads_es/ctlgproducto/mango.jpg',
+        image: 'https://memory-card123.s3-sa-east-1.amazonaws.com/images/mango-maduro-37797322.jpg',
         isClicked: false
     },
     {
         _id: '7',
         name: 'Kiwi',
-        image: 'https://img.freepik.com/foto-gratis/rebanada-kiwi-cerca_144627-8982.jpg?size=338&ext=jpg',
+        image: 'https://memory-card123.s3-sa-east-1.amazonaws.com/images/KWI-AgroReza.jpg',
         isClicked: false
     }, {
         _id: '8',
         name: 'Lemon',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXZdF3qSZR_dmwGwdBajkD6YGroKhVsHchUQ&usqp=CAU',
+        image: 'https://memory-card123.s3-sa-east-1.amazonaws.com/images/limon.jpg',
         isClicked: false
     }, {
         _id: '9',
         name: 'Strawberry',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtStCbCfJa0bc9laHgLwSvElsNYUI2kmSWsg&usqp=CAU',
+        image: 'https://memory-card123.s3-sa-east-1.amazonaws.com/images/strawberry.jpg',
         isClicked: false
     }, {
         _id: '10',
         name: 'Peach',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgLNP-3y3Zoiz0gLnxBT28GiferXJH7sBPSg&usqp=CAU',
+        image: 'https://memory-card123.s3-sa-east-1.amazonaws.com/images/peach.jpg',
         isClicked: false
     },
 ]
